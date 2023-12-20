2fbe2e20b7f45b5b8ee78966ced10afd99c6bcfa
Geek Code <rovafiti@email.com>
2023-12-20 15:45:42 +0300

    Feat: Implementer du système de création d'un bien ou (Ajout)

--- src/Controller/Admin/AdminPropertyController.php
+++ src/Controller/Admin/AdminPropertyController.php
@@ -41,7 +41,30 @@ class AdminPropertyController extends AbstractController
     }
 
     /**
-     * @Route("/admin/{id}" , name="admin.property.edit")
+     * @Route("/admin/property/create", name="admin.property.new")
+     * @return Response
+     */
+    public function new(Request $request): Response
+    {
+        $property = new Property() ;
+
+        $form = $this->createForm(PropertyType::class, $property) ;
+        $form->handleRequest($request) ;
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $this->em->persist($property);
+            $this->em->flush();
+            return $this->redirectToRoute('admin.property.index') ;
+        }
+
+        return $this->render('admin/property/new.html.twig', [
+            'property' => $property ,
+            'form' => $form->createView() ,
+        ]) ;
+    }
+
+    /**
+     * @Route("/admin/property/{id}" , name="admin.property.edit")
      * @param Property $property
      * @param Request $request
      * @return Response
